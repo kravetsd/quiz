c2463ab43cf13833b978e5c0d6f0ea4c40ef4a7d
kravetsd <kdykrg@gmail.com>
2023-01-19 20:50:41 +0300

    working on timer implementation

--- timer/timer.go
+++ timer/timer.go
@@ -2,10 +2,10 @@ package timer
 
 import (
 	"fmt"
-	"time"
 )
 
 func StartTimer() {
 	fmt.Println("Starting timer...")
-	fmt.Println(time.Now().Format("15:04:05"))
+
+	// stating timer in parallel to quiz questions
 }
