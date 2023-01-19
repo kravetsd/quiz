a92212b6d51b71124281b54282675587a1b1ea16
kravetsd <kdykrg@gmail.com>
2023-01-19 11:02:34 +0300

    part 1 finished, quiz is ready

--- docs/diagram.md
+++ docs/diagram.md
@@ -3,11 +3,12 @@
 title: quiz game flowchart
 ---
 flowchart TB
-    A[Start Quiz] ==> B[Read CSV]
+    A[Start Quiz] --> B[Read CSV]
     B --> C[Parse CSV]
     C --> D[Ask Question]
     D --> E[Check Answer]
-    E --> F[Next Question]
+    E --> F[Count Correct]
+    F --> G[Next Question]
     F --> G[End Quiz]
     G --> H[Print Results]
     H --> I[Exit]
--- main.go
+++ main.go
@@ -7,29 +7,67 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Starting quiz game...")
 
-	//Opening a csv file from local path
-	csvFile, err := os.Open("problems.csv")
+	// setting a flags to choose a file with quiz questions
+	flag.String("problem-file", "problems.csv", "path to csv file with quiz questions and answers")
+
+	// setting help flag and meesage
+	flag.String("h", "", "print this help message")
+
+	// parsing the flags
+	flag.Parse()
+
+	// getting the value of the flag
+	csvFilePath := flag.Lookup("problem-file").Value.String()
+
+	fmt.Println("Openning quiz from file:", csvFilePath)
+
+	// Opening a csv file from local path
+	csvFile, err := os.Open(csvFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// create a receiver for csv file
+	csvReader := csv.NewReader(csvFile)
+
+	// ask if user is ready to start the quiz
+	var ready string
+	fmt.Println("Are you ready to start the quiz? (y/n)")
+	fmt.Scanln(&ready) // getting answer from user via console
+	if ready != "y" {
+		fmt.Println("Ok, bye!")
+		return
+	}
+
+	//create a variable to store the score
+	var score int
+
+	// read all the records from csv file
+	recs, err := csvReader.ReadAll()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	data := csv.NewReader(csvFile)
-	records, err := data.ReadAll()
-
-	if err != nil {
-		log.Fatal(err)
+	for _, rec := range recs {
+		fmt.Println("Question:", rec[0], "?")
+		// getting answer from user via console
+		var answer string
+		fmt.Println("Answer: ")
+		fmt.Scanln(&answer)
+		if answer == rec[1] {
+			score++
+		}
 	}
 
-	// iterating over the records
-	for _, record := range records {
-		fmt.Println("question:", record[0], "\t answer:", record[1])
-	}
+	// print the score
+	fmt.Println("Your score is:", score, "from", len(recs))
+
 }
